f02ec3cab4083002f8badc3998ae97525cabc6e9
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-28 19:04:55 +0530

    Create instance variable

--- person.rb
+++ person.rb
@@ -1,7 +1,12 @@
 class Person
 
-    def initialize()
+    def initialize(name = 'Unknown', age, parent_permission = true)
+        @id = 
+        @name = name
+        @age = age
 
     end
+
+    
 
 end
