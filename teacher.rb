f42db38419192ef80b47761b5425d226a5efe140
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-28 19:24:48 +0530

    Create constructor for teacher class

--- teacher.rb
+++ teacher.rb
@@ -2,4 +2,9 @@ require "./person.rb"
 
 class Teacher < person
 
+    def initialize(name, age, parent_permission, specialization)
+        super(name, age, parent_permission)
+        @specialization = specialization
+    end
+
 end
