71ce59646ee797d261cebb9a7c76a375c5eb4b2e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-01 15:38:40 +0530

    Create instance of corrector class

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require './corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new()
   end
 
   attr_accessor :name, :age
