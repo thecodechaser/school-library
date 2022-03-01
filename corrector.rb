e77334c3a513cb2fb25a43d54827ebf7a41a229f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-01 15:35:16 +0530

    Add funtionality to correct_name function

--- corrector.rb
+++ corrector.rb
@@ -1,6 +1,11 @@
 class Corrector
 
-    def correct_name
-        
+    def correct_name(name)
+        corrected_name = name.capitalize()
+        if name.lentgh > 10
+            corrected_name = corrected_name.slice(0..10)
+        end
+         correct_name
     end
+    
 end
