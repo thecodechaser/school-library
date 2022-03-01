f5711451eb2b51159b5a8567b807b6d69a379fdf
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-01 15:47:32 +0530

    Fix linter erros

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new()
+    @corrector = Corrector.new
   end
 
   attr_accessor :name, :age
