ed13b66ec8b6538a32cc9c327095dc5bf2303fea
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-01 19:20:21 +0530

    Add method add_students in Classroom

--- classrroom.rb
+++ classrroom.rb
@@ -1,10 +1,14 @@
 class Classroom
     attr_accessor :label
-    
+    attr_reader :Students
+
     def initialize(label)
         @label = label
         @Students = []
     end
 
+    def add_student(student)
+        @student.push(student)
+    end
     
 end
