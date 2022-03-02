2da0754805ea0efe40e28e2cd422de69b7c32ef6
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-02 17:32:08 +0530

    Complete handle_input method

--- main.rb
+++ main.rb
@@ -30,17 +30,20 @@ end
 def handle_user_input(input)
     case input
     when 1
-
+        list_books()
     when 2
-
+        list_people()
     when 3
-
+        create_person()
     when 4
-
+        create_book()
     when 5
-
+        create_rental()
     when 6
-
+        list_rentals()
+    when 7 
+        puts "Thank you for using this app"
+        exit
     else puts "Please select a valid option"
         print_message()
     end
@@ -50,14 +53,13 @@ def print_message
     puts "\nPlease choose a option by entring a number
         1 - List all books
         2 - List all people
-        3 - Create a book
-        4 - Create a rental
-        5 - List all rentals for a given person id
-        6 - Exit"
-input = gets.chomp
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental
+        6 - List all rentals for a given person id
+        7 - Exit"
+input = gets.chomp.to_i
 handle_user_input(input)
-print_message()
-
 end
 
 def main
