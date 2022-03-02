a8a2177c1bd556174f3a26424f769fd3135fd962
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-02 15:26:28 +0530

    Update rental contsructor

--- rental.rb
+++ rental.rb
@@ -2,8 +2,13 @@ class Rental
     attr_accessor :date
     attr_reader :book, :person
 
-    def initialize(date)
+    def initialize(date, person, book)
         @date = date
+        @person = person
+        person.rentals << self
+        @book = book
+        book.rental << self
+
     end
 
     def book=(bookrental)
