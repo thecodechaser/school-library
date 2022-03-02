71a2fd66ac94af2e33355fb3ddaca07da32f433d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-02 15:12:35 +0530

    Add writer for rental class

--- rental.rb
+++ rental.rb
@@ -1,8 +1,12 @@
 class Rental
+    attr_accessor :date
 
     def initialize(date)
         @date = date
     end
 
-    attr_accessor :date
+    def book=(bookrental)
+        @rental= bookrental
+        bookrental.rentals.push(self) unless bookrental.rental.include?(self)
+    end
 end
