fdcf8ffb274a4079f99d40a576e0e348654d8fd5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-02 17:17:00 +0530

    Add method list_books

--- main.rb
+++ main.rb
@@ -3,14 +3,43 @@ require './teacher'
 require './book'
 require './rental'
 
+def list_books
+
+end
+
+
+
+def handle_user_input(input)
+    case input
+    when 1
+
+    when 2
+
+    when 3
+
+    when 4
+
+    when 5
+
+    when 6
+
+    else puts "Please select a valid option"
+        print_message()
+    end
+end
+
 def print_message
-    puts "Please choose a option by entring a number\n
-        1 - List all books\n
-        2 - List all people\n
-        3 - Create a book\n
+    puts "\nPlease choose a option by entring a number
+        1 - List all books
+        2 - List all people
+        3 - Create a book
         4 - Create a rental
-        5 - List all rentals for a given person id\n
-        6 - Exit"    
+        5 - List all rentals for a given person id
+        6 - Exit"
+input = gets.chomp
+handle_user_input(input)
+print_message()
+
 end
 
 def main
