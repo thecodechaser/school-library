26533212185b949939a91bc26ebb09cd2d9367bb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-07 15:43:31 +0530

    Add methods to App module

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-require './student'
-require './teacher'
-require './book'
-require './rental'
-
-Mdoule App
+module App
+  ENTER_MSG = 'Press ENTER to continue: '.freeze
+  INPUT_MSG = 'Enter an option number here: '.freeze
+  
   def list_books
     if @books.length.zero?
       puts "There's no book, Please add a book first"
@@ -83,24 +81,6 @@ Mdoule App
     end
   end
 
-  def create_person
-    puts "\nPlease choose a option by entring a number
-    1 -  Create a teacher
-    2 -  Create a student"
-    option = gets.chomp.to_i
-    case option
-    when 1
-      @persons.push(create_teacher)
-      puts 'Teacher created successfully!'
-    when 2
-      @persons.push(create_student)
-      puts 'Student created successfully!'
-    else
-      puts 'Please select a correct option'
-      create_person
-    end
-  end
-
   def create_book
     puts 'Enter book title'
     title = gets.chomp
@@ -114,55 +94,17 @@ Mdoule App
     puts 'Select a book by number '
     list_books
     print_message if @books.length.zero?
+    print "\n"+INPUT_MSG
     book_index = gets.chomp.to_i
     puts 'Select a person by number: '
     list_persons
     print_message if @persons.length.zero?
+    print "\n"+INPUT_MSG
     person_index = gets.chomp.to_i
     print 'Enter date: '
     date = gets.chomp
     Rental.new(date, @persons[person_index - 1], @books[book_index - 1])
     puts "Rental created succesfully -
     book: #{@books[book_index - 1].title}, Person: #{@persons[person_index - 1].name}, Date: #{date}"
-  end
-  # rubocop:disable Metrics/CyclomaticComplexity
-
-  def handle_input(option)
-    case option
-    when 1
-      list_books
-    when 2
-      list_persons
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thank you for using this app - Written by Ranjeet Singh'
-      exit
-    else puts 'Please select a valid option'
-         print_message
-    end
-  end
-  # rubocop:enable Metrics/CyclomaticComplexity
-
-  def print_message
-    puts "\nPlease choose a option by entring a number\n1 - List all books\n2 - List all people
-3 - Create a person\n4 - Create a book/n5 - Create a rental
-6 - List all rentals for a given person id\n7 - Exit"
-    option = gets.chomp.to_i
-    handle_input(option)
-    print_message
-  end
-
-  def run
-    @books = []
-    @persons = []
-    puts 'Welcome to school library! - Ranjeet Singh'
-    print_message
   end
 end
